76a62d51968522164405fac82b38c5f1c582b766
Tendry24 <hei.tendry.2@gmail.com>
2023-12-02 20:00:36 +0300

    fix: all tests removed

--- src/main/java/org/wallet/Main.java
+++ src/main/java/org/wallet/Main.java
@@ -5,9 +5,7 @@ import java.util.List;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    Crud find = new Crud();
-    List<Devise> devises = find.findAll();
-
-
-
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
