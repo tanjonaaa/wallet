416949aa02f60d7bd5d0dbedabd94d0eb9ac709a
tanjonaaa <hei.tanjona@gmail.com>
2023-12-03 17:11:18 +0300

    chore(CrudOperations<T>): added generic interface for CRUD operations

---
+++ src/main/java/org/wallet/CrudOperations/CrudOperations.java
@@ -0,0 +1,10 @@
+package org.wallet.CrudOperations;
+
+import java.util.List;
+
+public interface CrudOperations<T>{
+    List<T> findAll();
+    List<T> saveAll(List<T> toSave);
+    T save(T toSave);
+    T delete(T toDelete);
+}
