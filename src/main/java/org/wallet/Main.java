7c96447d0dcef846a7cfb0a298fb8180d3ae85ba
tanjonaaa <hei.tanjona@gmail.com>
2023-12-03 16:58:57 +0300

    chore: removed user informations in transcations

--- src/main/java/org/wallet/Main.java
+++ src/main/java/org/wallet/Main.java
@@ -1,18 +1,11 @@
 package org.wallet;
 
-import org.wallet.connectionDB.ConnectionDB;
-
 import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        ConnectionDB.getConnection();
-        ConnectionDB.getConnection();
-        ConnectionDB.getConnection();
-        ConnectionDB.getConnection();
-        ConnectionDB.closeConnection();
-        ConnectionDB.getConnection();
+        System.out.println("Hello world");
     }
 }
