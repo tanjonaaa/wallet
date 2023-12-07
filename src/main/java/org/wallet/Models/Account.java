933b429bbe82ebb2be99aa72aa086aa79f1d5c21
Tendry24 <hei.tendry.2@gmail.com>
2023-12-07 09:43:17 +0300

    fix: updated Account models , AccountComponents and CurrencyComponents

--- src/main/java/org/wallet/Models/Account.java
+++ src/main/java/org/wallet/Models/Account.java
@@ -2,6 +2,8 @@ package org.wallet.Models;
 
 import lombok.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 @RequiredArgsConstructor
@@ -12,7 +14,13 @@ import lombok.*;
 public class Account {
     private String accountId;
     @NonNull
+    private String name;
+    @NonNull
     private Double balance;
     @NonNull
     private String currencyId;
+    @NonNull
+    private List<Transaction> transactionList;
+    @NonNull
+    private String accountType;
 }
