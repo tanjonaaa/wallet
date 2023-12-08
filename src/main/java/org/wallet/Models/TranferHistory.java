92a82e2ee3096e7b7eed000789bd5f0842fd93d8
tanjonaaa <127602061+tanjonaaa@users.noreply.github.com>
2023-12-08 18:14:01 +0300

    feat: account money transfer
    * feat: added 2 methods(getAmountByDate,getBalanceHistory) on BalanceCrudOperations
    
    * feat: added new test on main class
    
    * fix: Changed the return type of the getAmountByDate
    
    * feat: add new migration and new model
    
    * fix: removed one unused function
    
    ---------
    
    Co-authored-by: Tendry24 <hei.tendry.2@gmail.com>
    Co-authored-by: Tendry_RAKOTOVAO <127602228+Tendry24@users.noreply.github.com>

---
+++ src/main/java/org/wallet/Models/TranferHistory.java
@@ -0,0 +1,22 @@
+package org.wallet.Models;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+public class TranferHistory {
+    private String transferHistoryId;
+    @NonNull
+    private String debitTransactionId;
+    @NonNull
+    private String creditTransactionId;
+
+    private LocalDateTime tranferDate;
+}
