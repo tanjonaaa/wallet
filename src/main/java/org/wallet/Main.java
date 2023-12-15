529c6c1e646f044590c31b1f23dc035411a9c10f
tanjonaaa <hei.tanjona@gmail.com>
2023-12-15 10:39:50 +0300

    feat: map CategoryAndSumAmount

--- src/main/java/org/wallet/Main.java
+++ src/main/java/org/wallet/Main.java
@@ -1,28 +1,29 @@
 package org.wallet;
 
 import org.wallet.CrudOperations.BalanceCrudOperations;
+import org.wallet.CrudOperations.CategoryCrudOperations;
 import org.wallet.CrudOperations.CurrencyCrudOperations;
 import org.wallet.CrudOperations.TransactionCrudOperations;
 import org.wallet.Models.Balance;
 import org.wallet.Models.Currency;
 import org.wallet.Models.Transaction;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        BalanceCrudOperations balanceCrudOperations = new BalanceCrudOperations();
+        var categoryCrudOperations = new CategoryCrudOperations();
+        var startDate = LocalDate.parse("2023-01-01");
+        var endDate = LocalDate.parse("2023-12-31");
 
-        String accountId = "d701dc54-911f-11ee-b9d1-0242ac120002";
-        LocalDateTime date = LocalDateTime.parse("2023-12-08T15:00");
-
-        Double totalAmount = balanceCrudOperations.getAmountByDate(accountId, date);
-
-        // Affichez le résultat dans la console
-        System.out.println("Le montant total est : " + totalAmount);
+        System.out.println(categoryCrudOperations.getSumOfTransactions(
+                "c9179a01-e126-4c96-96ad-92118a131349",
+                startDate,
+                endDate
+        ));
     }
 }
 
