9f484850e66432f436ff6a232d4557b9dee0a7a1
tanjonaaa <hei.tanjona@gmail.com>
2023-12-17 22:47:26 +0300

    feat: getAmountWithChangeRate

--- src/main/java/org/wallet/Main.java
+++ src/main/java/org/wallet/Main.java
@@ -22,20 +22,11 @@ public class Main {
         var accountCrud = new AccountCrudOperations();
         var balanceCrud = new BalanceCrudOperations();
 
-        System.out.println(balanceCrud.getAmountByDate("6f167b81-13dc-476d-a2a2-531b4b483cc0", LocalDateTime.now()));
-        System.out.println(balanceCrud.getAmountByDate("a103aa3d-60e2-4b65-b6e4-7254338ac5ff", LocalDateTime.now()));
+        System.out.println(balanceCrud.getAmountWithChangeRate(
+                "a103aa3d-60e2-4b65-b6e4-7254338ac5ff",
+                LocalDateTime.now()
+        ));
 
-        /*
-        System.out.println(accountCrud.makeTransaction(
-                new Transaction(
-                        "Kids",
-                        15_000d,
-                        TransactionType.EXPENSE,
-                        "6f167b81-13dc-476d-a2a2-531b4b483cc0",
-                        "1f35f7fd-2f52-4d8b-aab7-b7c0fae4597c"
-                )
-        ).toString());
-         */
     }
 }
 
